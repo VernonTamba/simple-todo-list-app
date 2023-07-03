f3943f2c273895aabf18f80038f6ec7c524f3dba
Vernon Tamba <vernon.tamba777@gmail.com>
2023-07-03 15:20:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "./AnimatedBg.css";
 
 function App() {
+  const [todoInput, setTodoInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* HEADER: Title, input, and add/submit button */}
+      <header className="app__header">
+        <h1 className="app__title">Todo List</h1>
+        <form className="app__inputForm">
+          <label htmlFor="todoItem" className="app__todoInputLabel">
+            What do you want todo?
+          </label>
+          <input
+            type="text"
+            id="todoItem"
+            className="app__todoInput"
+            value={todoInput}
+            onChange={(event) => setTodoInput(event.target.value)}
+          />
+        </form>
       </header>
+      {/* TODO LIST CONTAINER */}
+      <div className="app__todoListContainer">
+        <div className="app__todoListItem">
+          <p className="app__todoListItemTitle">Learning React</p>
+          <div className="app__todoListItemButtons">
+            <button className="app__button app__todoListItemCompleteButton">
+              ✅
+            </button>
+            <button className="app__button app__todoListItemDeleteButton">
+              🗑️
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* ANIMATED BACKGROUND from https://codepen.io/baarbaracrr/pen/KKovmGb */}
+      <div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+      </div>
     </div>
   );
 }
